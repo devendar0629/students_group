39222f2d2887bc4af574eaf63633f7bb4e10e328
devendar0629 <devendar0629@gmail.com>
2024-07-07 04:54:02 +0530

    Remove try catch block in connectDB function

--- src/DB/db.config.ts
+++ src/DB/db.config.ts
@@ -1,27 +1,19 @@
 import mongoose, { connect as mongooseConnect, MongooseError } from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        await mongooseConnect(process.env.MONGODB_URI!, {
-            dbName: process.env.DATABASE_NAME,
+    await mongooseConnect(process.env.MONGODB_URI!, {
+        dbName: process.env.DATABASE_NAME,
+    })
+        .then(() => {
+            console.log("🍀✅ MongoDB connection Succeeded.");
         })
-            .then(() => {
-                console.log("🍀✅ MongoDB connection Succeeded.");
-            })
-            .catch((err: any) => {
-                console.error("🍀❌ MongoDB connection failed.");
-                process.exit(1);
-            });
-
-        console.log("Connection length: ", mongoose.connections.length);
-
-        mongoose.connection.on("error", () => {
+        .catch((err: any) => {
             console.error("🍀❌ MongoDB connection failed.");
             process.exit(1);
         });
-    } catch (error: any) {
+
+    mongoose.connection.on("error", () => {
         console.error("🍀❌ MongoDB connection failed.");
-        console.log(error.name, error.message);
         process.exit(1);
-    }
+    });
 };
