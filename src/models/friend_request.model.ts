72017603b8b3d0aecc29cb46e7e732cab7b835d8
devendar0629 <devendar0629@gmail.com>
2024-07-30 20:07:41 +0530

    add friend request model

---
+++ src/models/friend_request.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { InferSchemaType } from "mongoose";
+
+const friendRequestSchema = new mongoose.Schema(
+    {
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        receiver: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export type TFriendRequest = InferSchemaType<typeof friendRequestSchema>;
+
+const FriendRequest =
+    (mongoose.models.FriendRequest as mongoose.Model<TFriendRequest>) ||
+    mongoose.model<TFriendRequest>("FriendRequest", friendRequestSchema);
+
+export default FriendRequest;
