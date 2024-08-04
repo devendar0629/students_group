80c72dcee805f0c77a0a6100ddfb43818a266792
devendar0629 <devendar0629@gmail.com>
2024-08-04 14:21:59 +0530

    add friend request component and a popup component for sending friend request

---
+++ src/app/(app)/friend-requests/friend_request.tsx
@@ -0,0 +1,129 @@
+"use client";
+
+import { useToast } from "@/components/ui/use-toast";
+import axios from "@/lib/config/axios.config";
+import { formatTimeAgo } from "@/utils/dateformatter";
+import { AxiosError } from "axios";
+
+interface FriendRequestProps {
+    friendRequest: any;
+    isSender: boolean;
+}
+
+const FriendRequest: React.FC<FriendRequestProps> = function ({
+    friendRequest,
+    isSender,
+}) {
+    const { toast } = useToast();
+
+    const acceptFriendRequest = async () => {
+        try {
+            const response = await axios.post(
+                "/api/v1/friend-requests/accept",
+                {
+                    friendRequestId: friendRequest._id,
+                }
+            );
+
+            if (response.status !== 201) {
+                toast({
+                    title: response.data.error.cause ?? "Error",
+                    description:
+                        response.data.error.message ?? "Something went wrong",
+                    variant: "destructive",
+                });
+            } else {
+                toast({
+                    title: "Friend request accepted successfully",
+                    className: "bg-green-700 text-slate-100",
+                });
+                setTimeout(() => window.location.reload(), 2000); // reload the page after 2 seconds
+            }
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                toast({
+                    title: error.response?.data.error.cause ?? "Error",
+                    description:
+                        error.response?.data.error.message ??
+                        "Something went wrong",
+                    variant: "destructive",
+                });
+            } else {
+                toast({
+                    title: "Error",
+                    description: "Something went wrong",
+                    variant: "destructive",
+                });
+            }
+        }
+    };
+    const rejectFriendRequest = async () => {
+        try {
+            const response = await axios.post(
+                "/api/v1/friend-requests/reject",
+                {
+                    friendRequestId: friendRequest._id,
+                }
+            );
+
+            if (response.status !== 200) {
+                toast({
+                    title: response.data.error.cause ?? "Error",
+                    description:
+                        response.data.error.message ?? "Something went wrong",
+                    variant: "destructive",
+                });
+            } else {
+                toast({
+                    title: "Friend request rejected successfully",
+                });
+                setTimeout(() => window.location.reload(), 2000); // reload the page after 2 seconds
+            }
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                toast({
+                    title: error.response?.data.error.cause ?? "Error",
+                    description:
+                        error.response?.data.error.message ??
+                        "Something went wrong",
+                    variant: "destructive",
+                });
+            } else {
+                toast({
+                    title: "Error",
+                    description: "Something went wrong",
+                    variant: "destructive",
+                });
+            }
+        }
+    };
+
+    return (
+        <>
+            <div className="flex flex-row flex-nowrap rounded-sm w-full py-2.5 px-5 justify-between items-center bg-sky-900">
+                <p>~{friendRequest.receiver.username}</p>
+                <div className="flex gap-1.5 flex-row mr-6 flex-nowrap items-center justify-center">
+                    {!isSender && (
+                        <button
+                            onClick={acceptFriendRequest}
+                            className="rounded-sm p-1 hover:bg-slate-400 bg-transparent"
+                        >
+                            ✅
+                        </button>
+                    )}
+                    <button
+                        onClick={rejectFriendRequest}
+                        className="rounded-sm p-1 hover:bg-slate-400 bg-transparent"
+                    >
+                        ❌
+                    </button>
+                </div>
+                <p className="text-sm font-light text-right">
+                    {formatTimeAgo(new Date(friendRequest.createdAt))}
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default FriendRequest;
---
+++ src/components/custom/Group/SendFriendRequestPopup.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
+import axios from "@/lib/config/axios.config";
+import { AxiosError } from "axios";
+import { Loader2Icon, UserRoundPlusIcon } from "lucide-react";
+import { useRef, useState } from "react";
+
+export default function SendFriendRequestPopup() {
+    const usernameRef = useRef<HTMLInputElement | null>(null);
+    const { toast } = useToast();
+    const [isSendingRequest, setIsSendingRequest] = useState<boolean>(false);
+
+    async function sendFriendRequest(e: React.FormEvent) {
+        try {
+            e.preventDefault();
+            setIsSendingRequest(true);
+            const usernameToSend = usernameRef.current?.value;
+            if (usernameToSend && usernameToSend.length < 2) {
+                toast({
+                    title: "Error",
+                    description: "Username should have atleast 2 characters",
+                    duration: 2000,
+                });
+                return;
+            }
+
+            const response = await axios.post("/api/v1/friend-requests", {
+                username: usernameToSend,
+            });
+
+            if (response.status !== 201) {
+                toast({
+                    title: "Error",
+                    description:
+                        response.data.error.message ?? "Something went wrong",
+                });
+            } else {
+                toast({
+                    title: "Success",
+                    description: "Friend request sent successfully",
+                    className: "bg-green-700 text-slate-100",
+                });
+            }
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                toast({
+                    title: "Error",
+                    description:
+                        error.response?.data.error.message ??
+                        "Something went wrong",
+                    variant: "destructive",
+                });
+                return;
+            }
+
+            toast({
+                title: "Error",
+                description: "Something went wrong",
+            });
+        } finally {
+            setIsSendingRequest(false);
+        }
+    }
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <p className="rounded-[50%] bg-transparent p-2.5 pb-[0.635rem] hover:bg-slate-500 text-2xl cursor-pointer">
+                    <UserRoundPlusIcon />
+                </p>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader className="gap-1">
+                    <DialogTitle className="lg:text-2xl text-xl font-semibold">
+                        Send friend request
+                    </DialogTitle>
+                    <DialogDescription className="pl-px text-[.9rem]">
+                        Enter the username of the user to send friend request
+                    </DialogDescription>
+                </DialogHeader>
+                <form
+                    onSubmit={sendFriendRequest}
+                    className="w-full h-full mt-2"
+                >
+                    <div className="flex flex-row flex-nowrap gap-3.5 items-center py-1.5 mb-4">
+                        <label>Username</label>
+                        <Input ref={usernameRef} />
+                    </div>
+
+                    <button
+                        disabled={isSendingRequest}
+                        className="text-[.95rem] px-4 py-[0.45rem] rounded-md bg-secondary hover:bg-secondary/80"
+                    >
+                        {isSendingRequest && (
+                            <Loader2Icon className="animate-spin mr-2 h-[20px] mb-[2px] inline-block align-bottom" />
+                        )}
+                        Send
+                    </button>
+                </form>
+            </DialogContent>
+        </Dialog>
+    );
+}
